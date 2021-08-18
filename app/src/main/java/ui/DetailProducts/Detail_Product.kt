6cf243a8f6124dd6a0c52f95a41415e26f51f3b2
Moh Dev <mezmoh530@gmail.com>
2021-08-18 18:25:08 +0100

    add in login ui

--- app/src/main/java/ui/DetailProducts/Detail_Product.kt
+++ app/src/main/java/ui/DetailProducts/Detail_Product.kt
@@ -2,11 +2,18 @@ package ui.DetailProducts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.ecommerce.R
+import android.animation.AnimatorSet
+
+import android.animation.ObjectAnimator
+
 
 class Detail_Product : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_product)
+
     }
 }
--- app/src/main/java/ui/Login/Login.kt
+++ app/src/main/java/ui/Login/Login.kt
@@ -1,12 +1,30 @@
 package ui.Login
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.ecommerce.R
+import ui.main.MainActivity
 
 class Login : AppCompatActivity() {
+
+    var logo:ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val bu_skip = findViewById<TextView>(R.id.tv_skip)
+
+        bu_skip.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
     }
+
+
 }
