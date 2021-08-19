356a3f9b5b5a627da0543e25d86f10953b9453ee
Moh Dev <mezmoh530@gmail.com>
2021-08-19 14:29:21 +0100

    add activity Profil.kt

---
+++ app/src/main/java/ui/Profil/Profil.kt
@@ -0,0 +1,12 @@
+package ui.Profil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecommerce.R
+
+class Profil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
